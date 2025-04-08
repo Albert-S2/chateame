4865707ab64dad3fa6789712edc220e661ff2a30
Albert Szupszynski <albert733844@gmail.com>
2025-04-08 22:05:43 +0100

    git actions added

--- app/page.js
+++ app/page.js
@@ -4,7 +4,8 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>Chateame Version 1.1</h1>
+        <h1>Chateame</h1>
+        <h2>Version 1.2</h2>
         <div className={styles.ctas}>
         </div>
       </main>
