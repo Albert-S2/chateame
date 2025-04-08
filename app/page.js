02cfac23d64bf97e9d52a5211b3c6b4b785e179e
Albert Szupszynski <albert733844@gmail.com>
2025-04-08 21:49:39 +0100

    change 4 - deployment test

--- app/page.js
+++ app/page.js
@@ -4,7 +4,7 @@ export default function Home() {
   return (
     <div className={styles.page}>
       <main className={styles.main}>
-        <h1>Chateame Version 1</h1>
+        <h1>Chateame Version 1.1</h1>
         <div className={styles.ctas}>
         </div>
       </main>
