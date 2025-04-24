49ef04beafde5bd9caefa2e1f1214208985dbe86
Albert Szupszynski <albert733844@gmail.com>
2025-04-24 18:59:47 +0100

    css for chatbot amended

--- app/components/chatbot.js
+++ app/components/chatbot.js
@@ -1,6 +1,6 @@
 'use client';
 import { useState, useEffect, useRef } from 'react';
-import './chatbot.css'; // Import your CSS file for styling
+import './chatbot.css';
 
 
 export default function Chatbot() {
